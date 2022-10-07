fedca9fb3c097a25affc581134e913e96bb6ebdb
doshmeister <me@jaydoshi.com>
2022-10-07 12:57:58 +0100

    kjhkjhkj

--- src/app.py
+++ src/app.py
@@ -1,7 +1,42 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.lang.builder import Builder
 
-class Cymbal(App):
+
+Builder.load_string("""
+<MainScreen>:
+    canvas.before:
+        Color:
+            rgba: .5,.5,.5,.5
+        Rectangle:
+            size: self.size
+            pos: self.pos
+    id: MapScreen
+    BoxLayout:
+        focus: True
+        orientation: "horizontal"
+        MainHeader:
+            InfoLabel:
+                text: "Cymbal AIS Viewer"
+        
+   """)
+
+
+class CymbalWidget(Widget):
+    pass
+
+class MainScreen(App):
     def build(self):
-        return Label(text="Hello World")
+        return CymbalWidget()
+
+    def on_touch_down(self,touch):
+        if super().on_touch_down(touch):
+            return True
+        if not self.collide_point(touch.x,touch.y):
+            return False
+
+
+
+if __name__ == "__main__":
+    MainScreen().run()
         
