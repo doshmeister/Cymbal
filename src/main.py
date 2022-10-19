bde04d5acefd47e45b684bcc771b6c18e030584d
doshmeister <me@jaydoshi.com>
2022-10-19 17:16:05 +0100

    xcvxcvxcv

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.lang.builder import Builder
+#from kivy.lang.builder import Builder
 from kivy.config import Config
-import time
+from kivy.clock import Clock
 import logging
 
-Config.window_icon = "data/icon.png"
-Config.set("input", "mouse", "mouse,disable_multitouch")
+#Fetches ais data with api key
+#ais.coordinates = doc.ais.something(something)
+
+
+#Sets icon in top left
+#Config.window_icon = "data/icon.png"
+#Sets input sources as mouse and disables touchscreen functionality (this prevents accidental inputs on trackpad)
+#Config.set("input", "mouse", "mouse,disable_multitouch")
 
 logger = logging.getLogger("logger")
 logging.basicConfig(format = "%(asctime)s - %(message)s",level=logging.INFO)
@@ -18,28 +24,25 @@ logging.basicConfig(format = "%(asctime)s - %(message)s",level=logging.CRITICAL)
 class MainScreen(Widget):
     pass
 
-class ship(Widget):
+class Shipobj(Widget):
     pass
     #def move(self):
-        #self.pos = "ais viewer api"
+        #self.pos = "ais api coordinates"
+        #self.direction = "ais api bearing"
 
 class CymbalApp(App):
     def build(self):
         return MainScreen()
 
-    def on_touch_down(self,touch):
+    #def on_touch_down(self,touch):z
         if super().on_touch_down(touch):
             return True
         if not self.collide_point(touch.x,touch.y):
             return False
     
-    
-
-
 if __name__ == "__main__":
     try:
         CymbalApp().run()
-        start = time.perf_counter()
     except Exception as e:
         logger.critical("Cymbal class failed to start")
-        pass
+        exit
