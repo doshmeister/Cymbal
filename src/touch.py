a1c4bd0f5d1f5062751f62a7b925b4243f666612
doshmeister <me@jaydoshi.com>
2023-03-07 09:49:52 +0000

    lkdfljdalg

---
+++ src/touch.py
@@ -0,0 +1,44 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+#from subprocess import Popen, PIPE
+import os, sys
+
+Builder.load_string("""
+<Screen>:
+    btn:btn
+    orientation: 'vertical'
+    Label:
+        id: msg
+        text: "Hi"
+        color: 1,0,0,1
+        pos_hint: {"top":0.8}
+
+    Button:
+        id: btn
+        size_hint: 0.2,0.2
+        text: "Touch Me"
+        on_release:  root.btn_touch_up()
+
+""")
+
+
+class Screen(BoxLayout):
+    btn = ObjectProperty(None)
+
+    def btn_touch_up(self):
+        print("Touch Up ")
+        #childprocess = Popen(['python3', 'settings.py',"r"], stdout=PIPE, stderr=PIPE)
+        #return childprocess
+        
+
+
+class TouchApp(App):
+    def build(self):
+        return Screen()
+
+
+if __name__ == "__main__":
+    TouchApp().run()
