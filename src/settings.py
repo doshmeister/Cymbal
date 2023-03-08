599f0afc2d8b1253bef91213f252f5a49109e0c0
doshmeister <me@jaydoshi.com>
2023-03-08 08:09:49 +0000

    oifjd

--- src/settings.py
+++ src/settings.py
@@ -1,24 +1,30 @@
+from kivy.uix.screenmanager import Screen as SettingScreen
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+import os
 
 
 kv = Builder.load_string("""
-<Screen>:
+<SettingScreen>:
     BoxLayout:
         orientation: 'vertical'
         Label:
             text: 'Setting Screen'
         Button:
+            id: btn
             text: 'Exit'
             on_release: app.stop()
 """)
 
 
-class SettingScreen(App):
-
+class SettingScreen(BoxLayout):
+    btn = ObjectProperty(None)
+        
+class SettingsApp(App):
     def build(self):
-        return kv
-
+        return SettingScreen()
 
 if __name__ == "__main__":
-    SettingScreen().run()
+    SettingsApp().run()
--- src/touch.py
+++ src/touch.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 #from subprocess import Popen, PIPE
@@ -12,14 +13,14 @@ Builder.load_string("""
     orientation: 'vertical'
     Label:
         id: msg
-        text: "Hi"
+        text: "by jones its working"
         color: 1,0,0,1
         pos_hint: {"top":0.8}
 
     Button:
         id: btn
         size_hint: 0.2,0.2
-        text: "Touch Me"
+        text: "Settings"
         on_release:  root.btn_touch_up()
 
 """)
@@ -32,6 +33,7 @@ class Screen(BoxLayout):
         print("Touch Up ")
         #childprocess = Popen(['python3', 'settings.py',"r"], stdout=PIPE, stderr=PIPE)
         #return childprocess
+
         
 
 
